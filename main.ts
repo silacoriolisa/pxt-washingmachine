fa5c0969acbab2161cdcd973cfcf6de400e98910
silacoriolisa <135693825+silacoriolisa@users.noreply.github.com>
2024-11-15 09:13:39 +0100

    Added Spin Cycle block

--- main.ts
+++ main.ts
@@ -5,11 +5,23 @@
  * generated for an **exported** function.
  */
 
-//% color="#AA278D" weight=100
-namespace Washing_Machine {
-    //% block
-    export function WM() {
-    
+//% color="#AA278D" weight=100 block="Wachine Machine"
+namespace WashingMachine {
+    //% group="Programs"
+    //% block="Spin Cycle"
+    //% weight=100
+    export function SpinCycle(speed: number): void {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+        basic.pause(2000)
+        StopMotorWithDelay()
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+        basic.pause(2000)
+        StopMotorWithDelay()
+    }
+
+    function StopMotorWithDelay(): void {
+        motor.motorStop(motor.Motors.M1)
+        basic.pause(2000)
     }
 
 
